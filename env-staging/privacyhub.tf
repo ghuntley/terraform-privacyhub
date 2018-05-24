c749a9d6ee82d7fffc231fc914540df3e8311214
Steve Huff <steve.huff@runkeeper.com>
2018-05-24 15:17:24 -0400

    fix: no http->https redirect

--- env-staging/privacyhub.tf
+++ env-staging/privacyhub.tf
@@ -49,14 +49,17 @@ resource "aws_iam_policy" "privacyhub_s3_rw" {
 }
 
 module "privacyhub" {
-  source                                 = "github.com/FitnessKeeper/terraform-aws-s3-cloudfront?ref=v1.1.0"
-  bucket_name                            = "${data.aws_s3_bucket.bucket.id}"
-  create_bucket                          = false
-  s3_region                              = "${data.aws_region.us-east-1.name}"
-  cloudfront_fqdn                        = "${local.fqdn}"
-  cloudfront_origin_path                 = "/${var.stack}"
-  cloudfront_origin_access_identity_path = "${var.cloudfront_origin_access_identity_path}"
-  cloudfront_price_class                 = "${var.price_class}"
-  cloudfront_acm_cert_domain             = "${local.tld}"
-  route53_toplevel_zone                  = "${local.tld}"
+  source                                                 = "github.com/FitnessKeeper/terraform-aws-s3-cloudfront?ref=v1.1.0"
+  bucket_name                                            = "${data.aws_s3_bucket.bucket.id}"
+  create_bucket                                          = false
+  s3_region                                              = "${data.aws_region.us-east-1.name}"
+  cloudfront_fqdn                                        = "${local.fqdn}"
+  cloudfront_origin_path                                 = "/${var.stack}"
+  cloudfront_origin_access_identity_path                 = "${var.cloudfront_origin_access_identity_path}"
+  cloudfront_price_class                                 = "${var.price_class}"
+  cloudfront_acm_cert_domain                             = "${local.tld}"
+  cloudfront_default_cache_compress                      = true
+  cloudfront_default_cache_viewer_protocol_policy        = "allow-all"
+  cloudfront_default_cache_forwarded_values_query_string = true
+  route53_toplevel_zone                                  = "${local.tld}"
 }
